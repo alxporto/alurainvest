22b7602c736145d5e5fd07d39046cf322faf76a5
datalastix <datalastix@gmail.com>
2023-09-12 21:17:50 -0300

    feat(account.py): create the the transfer(), get_balance() and __could_withdraw() new methods for the account class

--- alurainvest/account.py
+++ alurainvest/account.py
@@ -7,15 +7,46 @@ class Account:
         self.__balance = balance
         self.__limit = limit
 
-def statement(self):
-    print(f"Balance of {self.__balance:.2f} from {self.__owner}")
+    def statement(self):
+        print(f"Balance of {self.__balance:.2f} from {self.__owner}")
 
-def deposit(self, value):
-    self.__balance += value
+    def deposit(self, value):
+        self.__balance += value
 
-def withdraw(self, value):
-    self.__balance -= value
+    def __could_withdraw(self, value):
+        available_value = (self.__balance + self.__limit)
+        return value <= available_value
 
-def transfer(self, value, recipient):
-    self.withdraw(value)
-    recipient.deposit(value)
+    def withdraw(self, value):
+        if self.__could_withdraw(value):
+            self.__balance -= value
+        else:
+            print(f"The value {value} exceeded the limit")
+
+    def transfer(self, value, recipient):
+        self.withdraw(value)
+        recipient.deposit(value)
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @property
+    def limit(self):
+        return self.__limit
+    
+    @limit.setter
+    def limit(self, limit):
+        self.__limit = limit
+    
+    @staticmethod
+    def bank_code():
+        return "001"
+    
+    @staticmethod
+    def bank_codes():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
